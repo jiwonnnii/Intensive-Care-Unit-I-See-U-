5a6c70e5872197c554c32df95b67fb739ffbd87e
jiwonnnii <39324330+jiwonnnii@users.noreply.github.com>
2018-11-05 15:28:29 +0900

    Add files via upload

---
+++ master_bluetooth.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3); // RX | TX
+
+void setup() 
+{
+    Serial.begin(9600);
+    BTserial.begin(9600);  
+}
+ 
+void loop()
+{  
+    while (BTserial.available()) // 슬레이브로 부터 데이터 수신 대기
+    {  
+      Serial.write( BTserial.read()); // 물이 없으면 1
+    }
+  delay(20);
+
+ 
+}
---
+++ slave_bluetooth_Pulse_.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTserial(2, 3); // RX | TX
+ byte buffer[1024] ;
+ int num=0; 
+ 
+void setup() {
+  Serial.begin(9600);
+  BTserial.begin(9600);  
+
+  Wire.begin();
+  
+  // water
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT); 
+  //
+}
+void loop() {
+  int sensorVal = digitalRead(7);
+  
+  Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device 
+  while(Wire.available()) {          // slave may send less than requested
+    unsigned char c = Wire.read();   // receive heart rate value (a byte)
+
+    Serial.print(c, DEC);         // print heart rate value
+    Serial.print(",");
+    Serial.println(sensorVal);
+
+    if (sensorVal == HIGH) {
+      digitalWrite(13, LOW);    
+    } 
+    else {
+      digitalWrite(13, HIGH); 
+    }
+
+    BTserial.print(c);
+    BTserial.print(",");
+    BTserial.println(sensorVal);
+  }
+  delay(500);
+}
